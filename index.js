b474db6329ab8d3b3ba938236327d56742c64d71
Ismaile Pinheiro Rodrigues <ismailearlien@gmail.com>
2025-02-19 14:30:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const products = require('./products');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Configuração do multer para fazer upload de arquivos
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); // Nome único para o arquivo
+    }
+});
+
+const upload = multer({ storage });
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.post('/api/products', upload.single('image'), (req, res) => {
+    const newProduct = {
+        id: products.length + 1,
+        name: req.body.name,
+        price: parseFloat(req.body.price),
+        image: `/uploads/${req.file.filename}`
+    };
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+});
+
+app.delete('/api/products/:id', (req, res) => {
+    const productId = parseInt(req.params.id, 10);
+    const index = products.findIndex(product => product.id === productId);
+    if (index !== -1) {
+        products.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send({ message: 'Product not found' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
