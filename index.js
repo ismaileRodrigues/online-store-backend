2d9cd228e64e242a58613e2db3b72209aaa30a68
ismaileRodrigues <ismailearliene@gmail.com>
2025-02-19 23:20:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ app.delete('/api/products/:id', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on https://online-store-backend-three.vercel.app`);
 });
