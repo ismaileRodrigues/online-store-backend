11deb1b9fe0207749cae4d9ba8a1d96cc1e53a3b
ismaileRodrigues <ismailearliene@gmail.com>
2025-03-11 03:23:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Configuração do MongoDB
+const mongoose = require('mongoose');
+
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/online-store-backend';
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Conectado ao MongoDB');
-}).catch((err) => {
-    console.error('Erro ao conectar ao MongoDB', err);
-});
+
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log('Conectado ao MongoDB');
+    })
+    .catch((err) => {
+        console.error('Erro ao conectar ao MongoDB', err);
+    });
+
 
 // Definindo o modelo de Produto
 const productSchema = new mongoose.Schema({
